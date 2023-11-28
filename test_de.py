956889d74562977a7c817e20b89551b970228c8e
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-29 00:17:56 +0300

    changed 9

--- test_de.py
+++ test_de.py
@@ -37,7 +37,6 @@ def test_crossover(de_solver):
     cross_points = de_solver._crossover()
     assert len(cross_points) == de_solver.dimensions
     assert np.all(np.logical_or(cross_points, ~cross_points))
-    # Add more crossover tests as needed
 
 def test_recombination_and_evaluation(de_solver):
     de_solver._init_population()
@@ -48,9 +47,7 @@ def test_recombination_and_evaluation(de_solver):
         trial, trial_denorm = de_solver._recombination(idx)
         assert len(trial) == de_solver.dimensions
         assert np.all((trial >= 0) & (trial <= 1))
-        # Add more recombination tests as needed
         de_solver._evaluate(rastrigin(trial_denorm), idx)
-        # Add more evaluation tests as needed
 
 def test_iteration(de_solver):
     de_solver._init_population()
@@ -58,18 +55,5 @@ def test_iteration(de_solver):
     de_solver.iterate()
     assert de_solver.best is not None
     assert np.any(de_solver.best != initial_best)
-    # Add more iteration tests as needed
 
-def test_logging_info(de_solver, caplog):
-    de_solver._init_population()
-    de_solver.iterate()
-    # Check if info message is logged
-    assert "Инициализируем популяцию" in caplog.text
 
-def test_logging_error(de_solver, caplog):
-    de_solver._init_population()
-    # Intentionally set a fitness value exceeding 1e-3 to trigger the error_logger
-    de_solver.fitness[0] = 1e-2
-    de_solver._evaluate(1e-2, 0)
-    # Check if error message is logged
-    assert "Результат: 0.01, превышает 1e-3" in caplog.text
