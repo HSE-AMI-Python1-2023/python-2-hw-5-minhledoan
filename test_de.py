7b34fee25d9046b65d11f39eb2a06ee7dfa6490c
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-28 23:57:28 +0300

    changed 9

--- test_de.py
+++ test_de.py
@@ -1,42 +1,67 @@
 import pytest
 import numpy as np
+
 from differential_evolution import DifferentialEvolution
-import json
+
+BOUNDS_1 = np.array([[-20, 20], [-20, 20]])
+BOUNDS_2 = np.array([[-10, 50], [-10, 60]])
+BOUNDS_3 = np.array([[0, 110], [-42, 32]])
 
 def rastrigin(array, A=10):
     return A * 2 + (array[0] ** 2 - A * np.cos(2 * np.pi * array[0])) + (array[1] ** 2 - A * np.cos(2 * np.pi * array[1]))
 
-BOUNDS = np.array([[-20, 20], [-20, 20]])
 FOBJ = rastrigin
 
+
 @pytest.fixture
-def de_solver():
-    return DifferentialEvolution(FOBJ, BOUNDS)
+def de_solver_1():
+    return DifferentialEvolution(FOBJ, BOUNDS_1)
 
+@pytest.fixture
+def de_solver_2():
+    return DifferentialEvolution(FOBJ, BOUNDS_2)
 
-def test_info_coverage_json(de_solver):
-    de_solver._init_population()
-    filename = 'coverage.json'
-    de_solver.iterate()
-    de_solver._evaluate(rastrigin(de_solver.best), de_solver.best_idx)
-    de_solver._mutation()
-    de_solver._crossover()
-    trial, trial_denorm = de_solver._recombination(de_solver.best_idx)
+@pytest.fixture
+def de_solver_3():
+    return DifferentialEvolution(FOBJ, BOUNDS_3)
 
-    with open(filename, "r") as file:
-        data = json.loads(file.read())
+def test_initialization(de_solver_1):
+    assert de_solver_1.population_size > 0
+    assert de_solver_1.dimensions == 2
 
-    coverage_percentage = data['totals']['percent_covered']
-    assert coverage_percentage >= 100, f"Coverage is {coverage_percentage}%, expected at least 100%"
+def test_init_population(de_solver_1):
+    de_solver_1._init_population()
+    assert de_solver_1.population.shape == (de_solver_1.population_size, de_solver_1.dimensions)
+    assert np.all(de_solver_1.population >= 0) and np.all(de_solver_1.population <= 1)
 
-def test_best_solution_found(de_solver):
-    de_solver._init_population()
-    initial_best = de_solver.best
-    for _ in range(100):  # Run for 100 iterations
-        de_solver.iterate()
+def test_mutation(de_solver_1):
+    de_solver_1._init_population()
+    for idx in range(de_solver_1.population_size):
+        de_solver_1.idxs = [i for i in range(de_solver_1.population_size) if i != idx]
+        mutant = de_solver_1._mutation()
+        assert len(mutant) == de_solver_1.dimensions
 
-    assert np.all(rastrigin(de_solver.best) <= rastrigin(initial_best))
+def test_crossover(de_solver_1):
+    de_solver_1._init_population()
+    de_solver_1.idxs = list(range(de_solver_1.population_size))
+    de_solver_1._mutation()
+    cross_points = de_solver_1._crossover()
+    assert len(cross_points) == de_solver_1.dimensions
 
-def test_population_bounds(de_solver):
-    de_solver._init_population()
-    assert np.all(de_solver.population >= 0) and np.all(de_solver.population <= 1)
+def test_recombination_and_evaluation(de_solver_1):
+    de_solver_1._init_population()
+    for idx in range(de_solver_1.population_size):
+        de_solver_1.idxs = [i for i in range(de_solver_1.population_size) if i != idx]
+        de_solver_1._mutation()
+        de_solver_1._crossover()
+        trial, trial_denorm = de_solver_1._recombination(idx)
+        assert len(trial) == de_solver_1.dimensions
+        de_solver_1._evaluate(rastrigin(trial_denorm), idx)
+
+def test_iteration(de_solver_1):
+    de_solver_1._init_population()
+    initial_best = de_solver_1.best
+    de_solver_1.iterate()
+    assert de_solver_1.best is not None
+    assert de_solver_1.best is not initial_best or np.array_equal(de_solver_1.best, initial_best)
+
