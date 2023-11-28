eab8b70505e14c6fc4d533eb4503a8d50033f3f7
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-28 23:13:48 +0300

    changed 3

--- test_de.py
+++ test_de.py
@@ -18,3 +18,66 @@ FOBJ = rastrigin
 Запуск команды тестирования:
 pytest -s test_de.py --cov-report=json --cov
 """
+def test_initialization():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    assert de_solver.fobj == FOBJ
+    assert np.array_equal(de_solver.bounds, BOUNDS)
+    assert de_solver.mutation_coefficient == 0.8
+    assert de_solver.crossover_coefficient == 0.7
+    assert de_solver.population_size == 20
+    # Add more assertions based on your initialization logic
+
+def test_init_population():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    assert de_solver.population.shape == (20, 2)
+    assert np.all((de_solver.population >= 0) & (de_solver.population <= 1))
+    # Add more assertions based on your initialization logic
+
+def test_mutation():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    mutant = de_solver._mutation()
+    assert mutant.shape == (2,)
+    assert np.all((mutant >= 0) & (mutant <= 1))
+    # Add more assertions based on your mutation logic
+
+def test_crossover():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    cross_points = de_solver._crossover()
+    assert cross_points.shape == (2,)
+    assert all(val in [True, False] for val in cross_points)
+    # Add more assertions based on your crossover logic
+
+def test_recombination():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    de_solver._mutation()
+    de_solver._crossover()
+    trial, trial_denorm = de_solver._recombination(0)
+    assert trial.shape == (2,)
+    assert np.all((trial >= 0) & (trial <= 1))
+    # Add more assertions based on your recombination logic
+
+def test_evaluate():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    de_solver._mutation()
+    de_solver._crossover()
+    de_solver._recombination(0)
+    result_of_evolution = FOBJ(de_solver.trial_denorm)
+    de_solver._evaluate(result_of_evolution, 0)
+    assert result_of_evolution == de_solver.fitness[0]
+    # Add more assertions based on your evaluation logic
+
+def test_iterate():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS)
+    de_solver._init_population()
+    de_solver.iterate()
+    # Add assertions based on the expected behavior of the iterate method
+
+# Add more test cases as needed
+
+if __name__ == "__main__":
+    pytest.main()
