8152398b6226e78066dfca150231a2d6f6f2964d
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-29 19:23:14 +0300

    changed 9

--- test_de.py
+++ test_de.py
@@ -14,28 +14,33 @@ FOBJ = rastrigin
 def de_solver():
     return DifferentialEvolution(FOBJ, BOUNDS)
 
+
 def test_initialization(de_solver):
     assert de_solver.population_size > 0
     assert de_solver.dimensions == 2
 
+
 def test_init_population(de_solver):
     de_solver._init_population()
     assert de_solver.population.shape == (de_solver.population_size, de_solver.dimensions)
-    assert np.all(de_solver.population >= 0) and np.all(de_solver.population <= 1)
+    assert np.all((0 <= de_solver.population) & (de_solver.population <= 1))
+
 
 def test_mutation(de_solver):
     de_solver._init_population()
     for idx in range(de_solver.population_size):
         de_solver.idxs = [i for i in range(de_solver.population_size) if i != idx]
         mutant = de_solver._mutation()
-        assert len(mutant) == de_solver.dimensions
+        assert mutant.shape == (de_solver.dimensions,)
+
 
 def test_crossover(de_solver):
     de_solver._init_population()
-    de_solver.idxs = list(range(de_solver.population_size))  # Для примера, используем все индексы
+    de_solver.idxs = list(range(de_solver.population_size))
     de_solver._mutation()
     cross_points = de_solver._crossover()
-    assert len(cross_points) == de_solver.dimensions
+    assert cross_points.shape == (de_solver.dimensions,)
+
 
 def test_recombination_and_evaluation(de_solver):
     de_solver._init_population()
@@ -44,12 +49,13 @@ def test_recombination_and_evaluation(de_solver):
         de_solver._mutation()
         de_solver._crossover()
         trial, trial_denorm = de_solver._recombination(idx)
-        assert len(trial) == de_solver.dimensions
-        de_solver._evaluate(rastrigin(trial_denorm), idx)
+        assert trial.shape == (de_solver.dimensions,)
+        de_solver._evaluate(FOBJ(trial_denorm), idx)
+
 
 def test_iteration(de_solver):
     de_solver._init_population()
-    initial_best = de_solver.best
+    initial_best = de_solver.best.copy()
     de_solver.iterate()
     assert de_solver.best is not None
-    assert de_solver.best is not initial_best or np.array_equal(de_solver.best, initial_best)
+    assert not np.array_equal(de_solver.best, initial_best)
