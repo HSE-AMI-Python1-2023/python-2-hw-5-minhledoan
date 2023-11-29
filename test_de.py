9c1a2fa577392d270433184ee94f7a29ef1a35d4
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-29 23:01:28 +0300

    changed 9

--- test_de.py
+++ test_de.py
@@ -2,44 +2,54 @@ import pytest
 import numpy as np
 from differential_evolution import DifferentialEvolution
 
-# Rastrigin function
 def rastrigin(array, A=10):
     return A * 2 + (array[0] ** 2 - A * np.cos(2 * np.pi * array[0])) + (array[1] ** 2 - A * np.cos(2 * np.pi * array[1]))
 
-# Test configuration
 BOUNDS = np.array([[-20, 20], [-20, 20]])
 FOBJ = rastrigin
 
+# Fixture for DifferentialEvolution instance
 @pytest.fixture
 def de_solver():
     return DifferentialEvolution(FOBJ, BOUNDS)
 
-def test_initialization(de_solver):
+# Test cases
+def assert_initial(de_solver):
     assert de_solver.population_size > 0
     assert de_solver.dimensions == 2
 
-def test_init_population(de_solver):
+def test_population_initialization(de_solver):
     de_solver._init_population()
     assert de_solver.population is not None
     assert de_solver.population.shape == (de_solver.population_size, de_solver.dimensions)
     assert np.all((0 <= de_solver.population) & (de_solver.population <= 1))
+    assert np.array_equal(de_solver.min_bound, np.min(de_solver.bounds, axis=1))
+    assert np.array_equal(de_solver.max_bound, np.max(de_solver.bounds, axis=1))
+    assert np.array_equal(de_solver.diff, np.fabs(de_solver.min_bound - de_solver.max_bound))
+    expected_population_denorm = de_solver.min_bound + de_solver.population * de_solver.diff
+    assert np.array_equal(de_solver.population_denorm, expected_population_denorm)
+    expected_fitness = np.asarray([de_solver.fobj(ind) for ind in de_solver.population_denorm])
+    assert np.array_equal(de_solver.fitness, expected_fitness)
+    assert de_solver.best_idx == np.argmin(de_solver.fitness)
+    assert np.array_equal(de_solver.best, de_solver.population_denorm[de_solver.best_idx])
 
 def test_mutation(de_solver):
     de_solver._init_population()
     for idx in range(de_solver.population_size):
         de_solver.idxs = [i for i in range(de_solver.population_size) if i != idx]
         mutant = de_solver._mutation()
-        assert mutant is not None
         assert len(mutant) == de_solver.dimensions
-        assert np.all((0 <= mutant) & (mutant <= 1))
+        assert np.all((mutant >= 0) & (mutant <= 1))
 
 def test_crossover(de_solver):
     de_solver._init_population()
     de_solver.idxs = list(range(de_solver.population_size))
     de_solver._mutation()
-    cross_points = de_solver._crossover()
-    assert cross_points is not None
-    assert len(cross_points) == de_solver.dimensions
+    try:
+        cross_points = de_solver._crossover()
+        assert len(cross_points) == de_solver.dimensions
+    except IndexError:
+        assert de_solver.dimensions == 1
 
 def test_recombination_and_evaluation(de_solver):
     de_solver._init_population()
@@ -50,20 +60,12 @@ def test_recombination_and_evaluation(de_solver):
 
     for idx in range(de_solver.population_size):
         de_solver.idxs = [i for i in range(de_solver.population_size) if i != idx]
-        # Perform recombination and evaluation
         trial, trial_denorm = de_solver._recombination(idx)
-        assert trial is not None
-        assert trial_denorm is not None
-        # Ensure the shape of the trial vector
+        de_solver._evaluate(rastrigin(trial_denorm), idx)
         assert len(trial) == de_solver.dimensions
-        # Ensure trial vector values are either from the mutant or the current population
         assert np.all((trial == de_solver.mutant) | (trial == de_solver.population[idx]))
-        # Ensure trial_denorm is calculated correctly
-        assert de_solver.min_bound is not None
-        assert de_solver.max_bound is not None
         expected_trial_denorm = de_solver.min_bound + trial * de_solver.diff
         assert np.array_equal(trial_denorm, expected_trial_denorm)
-        # Ensure evaluation updates the fitness and population correctly
         result_of_evolution = rastrigin(trial_denorm)
         if result_of_evolution < de_solver.fitness[idx]:
             assert de_solver.fitness[idx] == result_of_evolution
@@ -78,3 +80,9 @@ def test_iteration(de_solver):
     de_solver.iterate()
     assert de_solver.best is not None
     assert de_solver.best is not initial_best or np.array_equal(de_solver.best, initial_best)
+
+def test_iteration_with_zero_population():
+    de_solver = DifferentialEvolution(FOBJ, BOUNDS, population_size=0)
+    de_solver.iterate()
+
+
