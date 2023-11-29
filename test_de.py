a8cc14600f0a3f368ffed06f2a8e8cf83403c9d6
LALALALISAAA <bl4ckh4wk2003@gmail.com>
2023-11-29 19:52:55 +0300

    changed 9

--- test_de.py
+++ test_de.py
@@ -15,18 +15,15 @@ def de_solver():
     return DifferentialEvolution(FOBJ, BOUNDS)
 
 def test_initialization(de_solver):
-    # Ensure that the solver is initialized with valid parameters
     assert de_solver.population_size > 0
     assert de_solver.dimensions == 2
 
 def test_init_population(de_solver):
-    # Ensure that the population is initialized with the correct shape
     de_solver._init_population()
     assert de_solver.population.shape == (de_solver.population_size, de_solver.dimensions)
     assert np.all((0 <= de_solver.population) & (de_solver.population <= 1))
 
 def test_mutation(de_solver):
-    # Ensure that mutation produces mutants with the correct shape
     de_solver._init_population()
     for idx in range(de_solver.population_size):
         de_solver.idxs = [i for i in range(de_solver.population_size) if i != idx]
@@ -34,7 +31,6 @@ def test_mutation(de_solver):
         assert mutant.shape == (de_solver.dimensions,)
 
 def test_crossover(de_solver):
-    # Ensure that crossover produces crossover points with the correct shape
     de_solver._init_population()
     de_solver.idxs = list(range(de_solver.population_size))
     de_solver._mutation()
@@ -42,7 +38,6 @@ def test_crossover(de_solver):
     assert cross_points.shape == (de_solver.dimensions,)
 
 def test_recombination_and_evaluation(de_solver):
-    # Ensure that recombination produces valid trials and evaluations
     de_solver._init_population()
     for idx in range(de_solver.population_size):
         de_solver.idxs = [i for i in range(de_solver.population_size) if i != idx]
@@ -53,17 +48,8 @@ def test_recombination_and_evaluation(de_solver):
         de_solver._evaluate(FOBJ(trial_denorm), idx)
 
 def test_iteration(de_solver):
-    # Ensure that the iteration updates the best solution
-    for _ in range(10):  # Run the test 10 times
-        de_solver._init_population()
-        initial_best = de_solver.best.copy()
-        de_solver.iterate()
-        assert de_solver.best is not None
-
-        # Check that the new solution is significantly different from the initial one
-        if not np.allclose(de_solver.best, initial_best, atol=1e-8):
-            break  # Success if the condition is met in any iteration
-    else:
-        # executed if the loop completes without a break (i.e., test consistently fails)
-        assert False, "Arrays are almost equal in multiple iterations."
-
+    de_solver._init_population()
+    initial_best = de_solver.best.copy()
+    de_solver.iterate()
+    assert de_solver.best is not None
+    assert not np.allclose(de_solver.best, initial_best, atol=1e-8), "Arrays are almost equal."
